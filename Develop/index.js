6e8c095f899d1399046069f800ecc5a5d3f2c1cc
Lian Liu <riceudon@gmail.com>
2022-12-21 00:04:19 -0800

    Added confirm message & error message for the app

--- Develop/index.js
+++ Develop/index.js
@@ -13,73 +13,79 @@ import { generateMarkdown } from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please provide a brief description of your project:'
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Please provide installation instructions for your project:'
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Please provide usage information for your project:'
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Please choose a license for your project:',
-      choices: [
-        'MIT',
-        'Apache 2.0',
-        'GPL 3.0',
-        'None'
-      ]
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Please provide contribution guidelines for your project:'
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Please provide test instructions for your project:'
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Please enter your GitHub username:'
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Please enter your email address:'
-    }
-  ];
-  
+  {
+    type: 'input',
+    name: 'title',
+    message: "What's the title of your project?"
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a brief description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions for your project:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage information for your project:'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please choose a license for your project:',
+    choices: [
+      'MIT',
+      'Apache 2.0',
+      'GPL 3.0',
+      'None'
+    ]
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Please provide contribution guidelines for your project:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Please provide test instructions for your project:'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Please enter your GitHub username:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Please enter your email address:'
+  }
+];
+
 
 
 // TODO: Create a function to write README file
 function writeToFile(readme) {
-    fs.writeFileSync(`README.md`, readme);
+  fs.writeFileSync(`README.md`, readme);
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then(answers => {
-    const readme = generateMarkdown(answers)
-    writeToFile(readme)
-  })
+  inquirer.prompt(questions)
+    .then(answers => {
+      const readme = generateMarkdown(answers);
+      writeToFile(readme)
+      console.log('Successfully created README file!')
+    })
+    .catch(err => {
+      console.error(`Error creating README file: ${err}`);
+    });
 };
+
 
 // Function call to initialize app
 init();
